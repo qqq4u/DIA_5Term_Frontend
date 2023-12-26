33a6af186aa8f9eb1b4d2429ec51ffc5c967de93
Данила Поляков <danila.polyakov@vk.team>
2023-12-26 11:20:52 +0300

    gh pages fixwedwed

--- src/Types.ts
+++
@@ -1,8 +0,0 @@
-export interface Parking {
-    id: number,
-    name: string,
-    address: string,
-    status: number,
-    image: string
-    places_count: number
-}
--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/DIA_5Term_Frontend/",
-  plugins: [react()],
-})
