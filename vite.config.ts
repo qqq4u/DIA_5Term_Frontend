d7170f3246b519585d908e3651208184d736f9f1
Данила Поляков <danila.polyakov@vk.team>
2023-12-26 11:37:15 +0300

    pages fixwe

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/DIA_5Term_Frontend/dist",
+  base: "/DIA_5Term_Frontend/src",
   plugins: [react()],
 })
