e62fe2e49ce4ab68a4191b8ebf9aec3e8689eca5
Данила Поляков <danila.polyakov@vk.team>
2023-12-26 10:30:09 +0300

    initial commit

---
+++ src/Components/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,75 @@
+import "./Breadcrumbs.sass"
+import { Link, useLocation } from "react-router-dom";
+import {FaChevronRight} from "react-icons/fa6";
+import {FaHome} from "react-icons/fa";
+
+const Breadcrumbs = ({ selectedParking, setSelectedParking }) => {
+
+    const location = useLocation()
+
+    let currentLink = ''
+
+    const topics: Record<string, string> = {
+        "parkings": "Парковки",
+        "profile": "Личный кабинет"
+    }
+
+    const resetSelectedParking = () => setSelectedParking(undefined)
+
+    const crumbs = location.pathname.split('/').filter(crumb => crumb !== '').map(crumb => {
+
+        currentLink += `/${crumb}`
+
+        if (Object.keys(topics).find(x => x == crumb))
+        {
+            return (
+                <div className={"crumb"} key={crumb}>
+
+                    <Link to={currentLink} onClick={resetSelectedParking}>
+                        { (topics as never)[crumb] }
+                    </Link>
+
+                    <FaChevronRight className={"chevron-icon"}/>
+
+                </div>
+            )
+        }
+
+        if (currentLink.match(new RegExp('parkings/(d*)')))
+        {
+            return (
+                <div className={"crumb"} key={crumb}>
+
+                    <Link to={currentLink}>
+                        { selectedParking?.name }
+                    </Link>
+
+                    <FaChevronRight className={"chevron-icon"}/>
+
+                </div>
+            )
+        }
+    });
+
+    return (
+        <div className="breadcrumbs-wrapper">
+            <div className="breadcrumbs">
+
+                <div className="crumb">
+
+                    <Link to={"/parkings"}>
+                        <FaHome className="home-icon" />
+                    </Link>
+
+                    <FaChevronRight className="chevron-icon" />
+
+                </div>
+
+                {crumbs}
+
+            </div>
+        </div>
+    )
+}
+
+export default Breadcrumbs;
