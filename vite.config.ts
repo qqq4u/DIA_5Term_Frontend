6ac99ec065fef8dd550af3f02886ce8abea3c4a5
Данила Поляков <danila.polyakov@vk.team>
2024-01-13 22:41:01 +0300

    gh pages fix3

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/bmstu/",
+  base: "/DIA_5Term_Frontend/",
   plugins: [react()],
 })
